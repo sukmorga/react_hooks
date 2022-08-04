6213d98a51b6463c76080da2a5a3d717d57308f0
sukmorga <naginks@outlook.com>
2022-08-04 19:50:43 +0200

    useInput

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import useInput from "./hooks/useInput";
 
 
 function App() {
-  return (
-    <div className="App">
-      Работает!
-    </div>
-  );
+    const username = useInput('');
+    const password = useInput('');
+
+
+    return (
+        <div>
+            <input {...username} type='text' placeholder="username" />
+            <input {...password} type='text' placeholder="password" />
+            <button onClick={() => console.log(username.value, password.value)}>Click</button>
+        </div>
+    );
 }
 
 export default App;
