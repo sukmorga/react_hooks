4786c07699349322b37c8e5a6be070c83b19c21a
sukmorga <naginks@outlook.com>
2022-08-06 02:38:36 +0200

    useRequest

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
+import axios from "axios";
 import { useState } from "react";
 import Hover from "./components/Hover";
 import List from "./components/List";
 import useDebounce from "./hooks/useDebounce";
 import useInput from "./hooks/useInput";
+import useRequest from "./hooks/useRequest";
 
 
 function App() {
-    const [value, setValue] = useState('');
-    const debounceSearch = useDebounce(search, 500);
+    const [todos, loading, error] = useRequest(fetchTodos);
 
 
-    function search(query) {
-        fetch(`https://jsonplaceholder.typicode.com/todos?query=${query}`)
-            .then(response => response.json())
-            .then(json => { console.log(json) })
+    function fetchTodos() {
+        return axios.get(`https://jsonplaceholder.typicode.com/todos`)
     }
 
-    const onChange = e => {
-        setValue(e.target.value);
-        debounceSearch(e.target.value);
-    };
+    if (loading) {
+        return <h1>Идет загрузка...</h1>
+    }
+
+    if (error) {
+        return <h1>Произошла ошибка при загрузке</h1>
+    }
 
 
     return (
         <div>
-            {/* <Hover />
-            <List /> */}
-            <input type='text' value={value} onChange={onChange} />
+            {todos && todos.map(todo =>
+                <div key={todo.id} style={{ padding: 10 }}>{todo.id}. {todo.title}</div>
+            )}
         </div>
     );
 }
