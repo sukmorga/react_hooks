4d6a0a25dd1e7c16845a4ed32bcb53986f331d28
sukmorga <naginks@outlook.com>
2022-08-06 02:20:18 +0200

    useDebounce

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
+import { useState } from "react";
 import Hover from "./components/Hover";
 import List from "./components/List";
+import useDebounce from "./hooks/useDebounce";
 import useInput from "./hooks/useInput";
 
 
 function App() {
+    const [value, setValue] = useState('');
+    const debounceSearch = useDebounce(search, 500);
 
+
+    function search(query) {
+        fetch(`https://jsonplaceholder.typicode.com/todos?query=${query}`)
+            .then(response => response.json())
+            .then(json => { console.log(json) })
+    }
+
+    const onChange = e => {
+        setValue(e.target.value);
+        debounceSearch(e.target.value);
+    };
 
 
     return (
         <div>
-            <Hover />
-            <List />
+            {/* <Hover />
+            <List /> */}
+            <input type='text' value={value} onChange={onChange} />
         </div>
     );
 }
